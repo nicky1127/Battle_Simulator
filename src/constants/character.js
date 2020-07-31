5f17ecc31fd8860f4c3c110b82150cbaded399d5
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-07-31 19:18:06 +0100

    add characters constants

---
+++ src/constants/character.js
@@ -0,0 +1,8 @@
+const characters = [
+  { code: 'pikachu', name: 'Pikachu', role: 'player' },
+  { code: 'ironman', name: 'Iron Man', role: 'player' },
+  { code: 'elsa', name: 'Elsa', role: 'player' },
+  { code: 'monster', Name: 'Mega Godzilla', role: 'monster' }
+];
+
+export default characters;
